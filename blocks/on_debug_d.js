a731ef8b8d98e549919bd07f3607f0664b65af1a
lepon01 <leondrolio@gmail.com>
2017-05-21 18:01:38 +0100

    not 'ready' but 'debug'

--- blocks/on_debug_d.js
+++ blocks/on_debug_d.js
@@ -1,6 +1,6 @@
 Blockly.JavaScript['on_debug'] = function(block) {
   var client_name = block.getFieldValue('client');
 
-  var code = `${client_name}.on('ready', console.log);`;
+  var code = `${client_name}.on('debug', console.log);`;
   return code;
 };
