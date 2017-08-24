30d223fc19b3a7a58b0ac1633a5a32d32050eaf0
lepon01 <leondrolio@gmail.com>
2017-08-24 22:36:40 +0100

    Add eval

--- js/discordblocks/builtins.js
+++ js/discordblocks/builtins.js
@@ -2,7 +2,7 @@ Blockly.Blocks.console_log = {
 	init() {
 		this.appendValueInput('text')
 			.setCheck(null)
-			.appendField('print');
+			.appendField('log');
 		this.setPreviousStatement(true, null);
 		this.setNextStatement(true, null);
 		this.setColour(160);
@@ -15,6 +15,26 @@ Blockly.JavaScript.console_log = (block) => {
 	const value_text = Blockly.JavaScript.valueToCode(block, 'text', Blockly.JavaScript.ORDER_ATOMIC);
 	// TODO: Assemble JavaScript into code variable.
 	const code = `console.log(${value_text});\n`;
+	return code;
+};
+
+Blockly.Blocks.console_log = {
+	init() {
+		this.appendValueInput('text')
+			.setCheck(null)
+			.appendField('eval');
+		this.setPreviousStatement(true, null);
+		this.setNextStatement(true, null);
+		this.setColour(160);
+		this.setTooltip('The eval() function evaluates JavaScript code represented as a string.');
+		this.setHelpUrl('https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/eval');
+	}
+};
+
+Blockly.JavaScript.console_log = (block) => {
+	const value_text = Blockly.JavaScript.valueToCode(block, 'text', Blockly.JavaScript.ORDER_ATOMIC);
+	// TODO: Assemble JavaScript into code variable.
+	const code = `eval(${value_text});\n`;
 	return code;
 };
 
