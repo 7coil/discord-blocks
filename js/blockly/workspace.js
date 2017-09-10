872e640a8f9c3446d13fa367a8c3e89f86a52171
lepon01 <leondrolio@gmail.com>
2017-09-10 01:54:35 +0100

    Keep scrolling to (0, 0)

--- js/blockly/workspace.js
+++ js/blockly/workspace.js
@@ -2,3 +2,7 @@ const blocklyDiv = document.getElementById('discordblocks');
 const workspace = Blockly.inject(blocklyDiv, {
 	toolbox: document.getElementById('toolbox')
 });
+
+setInterval(() => {
+	scrollTo(0, 0);
+}, 50);
