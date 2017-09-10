4a12f8f80a29a68a57852a4f7668c4c9efe0ea90
lepon01 <leondrolio@gmail.com>
2017-09-10 01:49:51 +0100

    Use CSS only for resize

--- js/blockly/workspace.js
+++ js/blockly/workspace.js
@@ -1,31 +1,4 @@
-const blocklyArea = document.getElementById('discordblocksarea');
 const blocklyDiv = document.getElementById('discordblocks');
 const workspace = Blockly.inject(blocklyDiv, {
 	toolbox: document.getElementById('toolbox')
 });
-
-const resize = () => {
-	// Compute the absolute coordinates and dimensions of blocklyArea.
-	let element = blocklyArea;
-	let x = 0;
-	let y = 0;
-	do {
-		x += element.offsetLeft;
-		y += element.offsetTop;
-		element = element.offsetParent;
-	} while (element);
-
-	blocklyArea.style.height = `calc(100vh - ${y}px)`;
-
-	// Position blocklyDiv over blocklyArea.
-	blocklyDiv.style.left = `${x}px`;
-	blocklyDiv.style.top = `${y}px`;
-	blocklyDiv.style.width = `${blocklyArea.offsetWidth}px`;
-	blocklyDiv.style.height = `${blocklyArea.offsetHeight}px`;
-};
-
-window.addEventListener('resize', resize, false);
-setTimeout(resize, 50);
-
-
-Blockly.svgResize(workspace);
