afde2628a8bf677a0b10d6e0f01e6d2aa0d4522b
Katie <katie@discordmail.com>
2019-08-02 15:14:29 +0100

    Not importing any of Blockly

--- src/modules/Blockly.js
+++ src/modules/Blockly.js
@@ -5,6 +5,15 @@ const script = `
   ${fs.readFileSync(__dirname + '/blockly/blockly_compressed.js', {
     encoding: 'utf-8'
   })}
+  ${fs.readFileSync(__dirname + '/blockly/blocks_compressed.js', {
+    encoding: 'utf-8'
+  })}
+  ${fs.readFileSync(__dirname + '/blockly/msg/js/en-gb.js', {
+    encoding: 'utf-8'
+  })}
+  ${fs.readFileSync(__dirname + '/blockly/javascript_compressed.js', {
+    encoding: 'utf-8'
+  })}
   return Blockly;
 })()
 `
