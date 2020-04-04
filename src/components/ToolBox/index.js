e5e95d68447675fdbbd5e2754276b392cb5f8a77
Leondro Lio <github@leondrolio.com>
2020-04-04 03:48:16 +0100

    Temporarily remove starter category and move advanced blocks outside

--- src/components/ToolBox/index.js
+++ src/components/ToolBox/index.js
@@ -2,18 +2,16 @@ import React from 'react';
 import Category from './Category';
 import CoreCategory from './categories/CoreCategory';
 import categoryDefinitions from './categories/DiscordBlocks/autogen';
-import StarterCategory from './categories/DiscordBlocks/StarterCategory';
+// import StarterCategory from './categories/DiscordBlocks/StarterCategory';
 
 const Tools = (props, ref) => (
   <xml is="xml" ref={ref}>
     <CoreCategory />
     <category is="category" name="Variables" custom="VARIABLE"></category>
     <category is="category" name="Functions" custom="PROCEDURE"></category>
-    <Category blocks={StarterCategory} name="Discord.js">
-      <category is="category" name="For Pro Developers">
-        {categoryDefinitions.map(category => <Category key={category.name} blocks={category} />)}
-      </category>
-    </Category>
+    {/* <Category blocks={StarterCategory} name="Discord.js" /> */}
+    <sep />
+    {categoryDefinitions.map(category => <Category key={category.name} blocks={category} />)}
   </xml>
 )
 
